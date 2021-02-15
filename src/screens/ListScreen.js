aade6cafa61828db1d288047516efaf6e672935e
waterrune <srune198@gmail.com>
2021-02-16 04:46:10 +0800

    keys and flat list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, FlatList } from "react-native";
 
 const ListScreen = () => {
-  return <Text>List Screen</Text>;
+  const friends = [
+    { name: "Friend #1", key: "1" },
+    { name: "Friend #2", key: "2" },
+    { name: "Friend #3", key: "3" },
+    { name: "Friend #4", key: "4" },
+    { name: "Friend #5", key: "5" },
+    { name: "Friend #6", key: "6" },
+    { name: "Friend #7", key: "7" },
+    { name: "Friend #8", key: "8" },
+    { name: "Friend #9", key: "9" },
+  ];
+  return (
+    <FlatList
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text>{item.name}</Text>;
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({});
